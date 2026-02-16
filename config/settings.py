f7a733e0603f9421207f23cfb7f43b72be8705eb
Balla Krisztian <skicpausz@sztaki.hu>
2026-02-16 11:26:51 +0100

    Add: PostgresSQL DB

--- config/settings.py
+++ config/settings.py
@@ -80,18 +80,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL"):
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photoalbumdb',
+        'USER': 'photoalbum',
+        'PASSWORD': 'photoalbum123',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+
 
 
 
